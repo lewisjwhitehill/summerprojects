798e8fb488cba54c4a65b387d78bb67af350031f
Lewis Whitehill <lwhitehi@ucsc.edu>
2024-08-16 15:49:52 -0700

    changed vite.config to try and fix a bug

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/summerprojects/',
+  base: '/dist/',
   plugins: [react()],
+  build: {
+    outDir: 'dist' // This is the default value
+  },
 })
