03c4aae2bc364025483179abaf1cda66029d3976
Lewis Whitehill <lwhitehi@ucsc.edu>
2024-08-21 16:09:15 -0700

    making sure git is up to date

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/dist/',
+  base: '/summerprojects/src/dist/',
   plugins: [react()],
   build: {
     outDir: 'dist' // This is the default value
