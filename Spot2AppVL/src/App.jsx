7be1e2972dedca8f14dcdfe59de8b35a0bf4507c
vharkins <105030530+vharkins1@users.noreply.github.com>
2024-09-04 12:42:00 -0700

    trying to fix App, adding router dom seems to have caused issues

--- Spot2AppVL/src/App.jsx
+++ Spot2AppVL/src/App.jsx
@@ -1,9 +1,8 @@
-// src/App.jsx
 import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Switch, useLocation } from 'react-router-dom';
 import './App.css';
-import LoginWithSpotify from './components/LoginWithSpotify.jsx'; // Spotify login component
-import Dashboard from './components/Dashboard.jsx'; // New Dashboard component
-import { useLocation } from 'react-router-dom';
+import LoginWithSpotify from './components/LoginWithSpotify'; // Spotify login component
+import Dashboard from './components/Dashboard'; // New Dashboard component
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false); // Track whether user is logged in
@@ -14,7 +13,7 @@ function App() {
   useEffect(() => {
     const params = new URLSearchParams(location.search);
     const token = params.get('access_token');
-    
+
     if (token) {
       setAccessToken(token);
       setIsLoggedIn(true); // Set login state to true if token is present
@@ -22,13 +21,18 @@ function App() {
   }, [location]);
 
   return (
-    <div className="App">
-      {isLoggedIn ? (
-        <Dashboard accessToken={accessToken} /> // Show dashboard after login
-      ) : (
-        <LoginWithSpotify /> // Show login screen before login
-      )}
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/dashboard">
+            {isLoggedIn ? <Dashboard accessToken={accessToken} /> : <LoginWithSpotify />}
+          </Route>
+          <Route path="/">
+            <LoginWithSpotify />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
