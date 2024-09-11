d4278658e2c7c814101647f15e101d131b64fdfb
vharkins <105030530+vharkins1@users.noreply.github.com>
2024-09-11 14:15:26 -0700

    Got rid of Routing in App.jsx

--- Spot2AppVL/src/App.jsx
+++ Spot2AppVL/src/App.jsx
@@ -1,43 +1,31 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import LoginWithSpotify from "./components/LoginWithSpotify.jsx";
-import Dashboard from "./components/Dashboard.jsx"; 
-import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom'; 
+import Dashboard from "./components/Dashboard.jsx";
 
 function App() {
   const [accessToken, setAccessToken] = useState(null);
 
-  // Function to extract query parameters from the URL
-  const useQuery = () => {
-    return new URLSearchParams(useLocation().search);
-  };
-
+  // Function to extract the access token from the URL query parameters
   useEffect(() => {
-    const query = useQuery();
-    const token = query.get("access_token");
+    const queryParams = new URLSearchParams(window.location.search);
+    const token = queryParams.get("access_token");
 
     if (token) {
       setAccessToken(token);
-      // Optionally, you can remove the token from the URL after setting it
+      // Optionally, you can remove the token from the URL once it's set
       window.history.replaceState({}, document.title, "/");
     }
   }, []);
 
-
   return (
-    <AuthContext.Provider value={accessToken}>
-      <Router>
-        <div className="card">
-          <Routes>
-            {accessToken ? (
-              <Route path="/" element={<Dashboard />} />
-            ) : (
-              <Route path="/" element={<LoginWithSpotify onLogin={setAccessToken} />} />
-            )}
-          </Routes>
-        </div>
-      </Router>
-    </AuthContext.Provider>
+    <div className="card">
+      {accessToken ? (
+        <Dashboard />
+      ) : (
+        <LoginWithSpotify onLogin={setAccessToken} />
+      )}
+    </div>
   );
 }
 
