063446d5711f96fce0cbf4cb3435d8e7abe5c7cc
vharkins <105030530+vharkins1@users.noreply.github.com>
2024-09-11 14:08:49 -0700

    added console log to dashboard

--- Spot2AppVL/src/components/Dashboard.jsx
+++ Spot2AppVL/src/components/Dashboard.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React from "react";
 
 function Dashboard() {
+  console.log("Dashboard rendered");
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
       <h1>Thanks for Connecting </h1>
