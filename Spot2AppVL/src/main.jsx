34385422332bf53444e801778ea0f53552dd26fc
vharkins <105030530+vharkins1@users.noreply.github.com>
2024-09-12 15:35:22 -0700

    trying to get the app side by side

--- Spot2AppVL/src/main.jsx
+++ Spot2AppVL/src/main.jsx
@@ -5,9 +5,10 @@ import App from './App.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
- 
-    <StrictMode>
+  <StrictMode>
+    <div style={{ display: 'flex' }}>
       <App />
       <App />
-    </StrictMode>,
-)
+    </div>
+  </StrictMode>
+);
