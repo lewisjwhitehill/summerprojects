14adb7cd381cc12cd6ed310b3c7e514b0418bdd5
vharkins <105030530+vharkins1@users.noreply.github.com>
2024-12-15 16:29:05 -0800

    adding more scopes to spotify

--- Spot2AppVL/api/spotify.js
+++ Spot2AppVL/api/spotify.js
@@ -1,7 +1,16 @@
 export default function handler(req, res) {
     const clientId = process.env.SPOTIFY_CLIENT_ID;
     const redirectUri = process.env.SPOTIFY_REDIRECT_URI;
-    const scopes = 'user-read-private user-read-email playlist-read-private playlist-read-collaborative user-library-read user-top-read';
+    const scopes = [
+      'user-read-private',
+      'user-read-email',
+      'playlist-read-private',
+      'playlist-read-collaborative',
+      'user-library-read',
+      'user-top-read',
+      'playlist-modify-private', // Added
+      'playlist-modify-public'   // Added
+    ].join(' ');
   
     // Log the values for debugging
     console.log('Client ID:', clientId);
